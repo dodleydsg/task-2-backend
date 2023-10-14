e11d6f765c427f76cc235b62bbff8a6a670209af
Dodley <dodleydesign+github@gmail.com>
2023-10-14 10:57:36 +0100

    Added error boundaries to mutations

--- app.js
+++ app.js
@@ -20,16 +20,24 @@ const typeDefs = `#graphql
 
     type AddProductResponse{
       success: Boolean,
-      product: Product
+      product: Product,
+      message: String
     }
     type DeleteProductResponse{
       success: Boolean,
-      product: Product
+      product: Product,
+      message: String
+    }
+    input AddProductInput{
+      title: String!,
+      imageUrl: String!,
+      amount: Int!,
+      currency: String!
     }
 
     type Mutation{
         "Add a product to the database"
-        addProduct(title: String!, imageUrl: String!, amount: Int!, currency: String!): AddProductResponse
+        addProduct(values: AddProductInput): AddProductResponse
         "Deletes a product and returns its object"
         deleteProduct(id:ID!): DeleteProductResponse
     }
@@ -43,21 +51,38 @@ const resolvers = {
       await prisma.product.findUnique({ where: { id: args.id } }),
   },
   Mutation: {
-    addProduct: async (parent, args) => {
+    addProduct: async (parent, {values}) => {
       const product = await prisma.product.create({
-        data: { ...args, amount: Number(args.amount) },
+        data: { ...values, amount: Number(values.amount) },
       });
-      return {
-        success: 200,
-        product
+      let message;
+      let success;
+      if (product) {
+        message = "Successfully added a new product";
+        success = true;
+      }else{
+        message = "Error adding a new product",
+        success = false 
       }
+      return {
+        message,
+        success,
+        product,
+      };
     },
     deleteProduct: async (parent, args) => {
       const product = await prisma.product.delete({ where: { id: args.id } });
+      if(!product){
+        return {
+          message: "An error occured while deleting the product",
+          success: false,
+          product: null
+        }
+      }
       return {
         success: 200,
-        product
-      }
+        product,
+      };
     },
   },
 };
